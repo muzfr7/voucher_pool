eec760d49eed9a22f05a057c7c9778996245f820
Muzafar Ali <muzfr7@gmail.com>
2021-07-13 09:01:23 +0500

    register customer service and handler and add route for it

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,7 +4,12 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	customerDomain "github.com/muzfr7/voucher_pool/app/domain/customer"
+
+	repository "github.com/muzfr7/voucher_pool/app/infrastructure/repository"
+	customerInterface "github.com/muzfr7/voucher_pool/app/interfaces/customer"
 	pingInterface "github.com/muzfr7/voucher_pool/app/interfaces/ping"
+	customerUsecase "github.com/muzfr7/voucher_pool/app/usecases/customer"
 )
 
 var (
@@ -21,19 +26,27 @@ func init() {
 
 func main() {
 
+	// instantiate repositories
+	customerRepo := repository.NewCustomerRepository()
+
+	// instantiate services
+	customerSVC := customerUsecase.NewService(customerRepo)
+
 	// instantiate handlers
 	pingHandler := pingInterface.NewHandler()
+	customerHandler := customerInterface.NewHandler(customerSVC)
 
 	// create routes and run server
-	createRoutes(pingHandler)
+	createRoutes(pingHandler, customerHandler)
 	runServer()
 }
 
 // createRoutes will define app routes.
-func createRoutes(pHandler pingInterface.Handler) {
+func createRoutes(pHandler pingInterface.Handler, cHandler customerDomain.Handler) {
 	v1 := router.Group(baseRouteV1)
 	{
 		v1.GET("ping", pHandler.Ping)
+		v1.POST("customers", cHandler.Create)
 	}
 }
 
