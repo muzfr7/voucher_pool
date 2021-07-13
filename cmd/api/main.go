c0f2e29246a0a87bef2b2b0cf8812b31f01f33ec
Muzafar Ali <muzfr7@gmail.com>
2021-07-13 20:50:42 +0500

    update cmd/api/main to load environment variables into env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kelseyhightower/envconfig"
 	customerDomain "github.com/muzfr7/voucher_pool/app/domain/customer"
+	infraMySQL "github.com/muzfr7/voucher_pool/app/infrastructure/mysql"
+	"github.com/muzfr7/voucher_pool/environmentconfig"
 
 	repository "github.com/muzfr7/voucher_pool/app/infrastructure/repository"
 	customerInterface "github.com/muzfr7/voucher_pool/app/interfaces/customer"
@@ -16,18 +20,39 @@ var (
 	// baseRoute will be used by all routes.
 	baseRouteV1 string
 
+	// env will contain envs.
+	env environmentconfig.EnvironmentConfig
+
 	// creates a gin router with default middleware: logger and recovery (crash-free) middleware.
 	router = gin.Default()
 )
 
 func init() {
 	baseRouteV1 = "/api/v1/"
+
+	// load environment variables from .env file if present
+	if _, err := os.Stat("./.env"); err == nil {
+		if err = environmentconfig.Export("./.env"); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// load environment variables into EnvironmentConfig struct
+	if err := envconfig.Process("", &env); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
+	// instantiate mysql connection
+	dbConn := infraMySQL.NewConnection(env.DBDriver, env.DBUser, env.DBPass, env.DBHost, env.DBPort, env.DBName)
+	db, err := dbConn.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// instantiate repositories
-	customerRepo := repository.NewCustomerRepository()
+	customerRepo := repository.NewCustomerRepository(db)
 
 	// instantiate services
 	customerSVC := customerUsecase.NewService(customerRepo)
