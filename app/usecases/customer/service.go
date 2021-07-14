def51b01aa5758693ad95d05043af679c4b3d03b
Muzafar Ali <muzfr7@gmail.com>
2021-07-14 17:04:29 +0500

    add create method in customer service

--- app/usecases/customer/service.go
+++ app/usecases/customer/service.go
@@ -15,3 +15,17 @@ func NewService(r customerDomain.Repository) customerDomain.Service {
 		repo: r,
 	}
 }
+
+// Create will create a new customer.
+func (s *service) Create(payload *customerDomain.CreateCustomerRequestDTO) error {
+	// store customer in the database
+	err := s.repo.Create(&customerDomain.Customer{
+		Name:  payload.Name,
+		Email: payload.Email,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
