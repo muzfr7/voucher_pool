f8acc4262a05d232456d61e269f732e0ec04484d
Muzafar Ali <muzfr7@gmail.com>
2021-07-14 17:05:54 +0500

    update create handler logic to parse, validate json and call customer svc create method

--- app/interfaces/customer/handler.go
+++ app/interfaces/customer/handler.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	customerDomain "github.com/muzfr7/voucher_pool/app/domain/customer"
+	"github.com/muzfr7/voucher_pool/app/interfaces"
 )
 
 // handler implements customerDomain.Handler interface.
@@ -21,5 +22,24 @@ func NewHandler(s customerDomain.Service) customerDomain.Handler {
 
 // Create will handle customer creation.
 func (h *handler) Create(c *gin.Context) {
+	// decode json body into payload
+	var payload = &customerDomain.CreateCustomerRequestDTO{}
+	if err := c.ShouldBindJSON(payload); err != nil {
+		c.JSON(http.StatusBadRequest, interfaces.NewBadRequestError("invalid JSON body"))
+		return
+	}
+
+	// validate payload data
+	if err := payload.Validate(); err != nil {
+		c.JSON(http.StatusBadRequest, interfaces.NewBadRequestError(err.Error()))
+		return
+	}
+
+	// create new customer
+	if err := h.customerSVC.Create(payload); err != nil {
+		c.JSON(http.StatusInternalServerError, interfaces.NewInternalServerError(err.Error()))
+		return
+	}
+
 	c.JSON(http.StatusCreated, nil)
 }
